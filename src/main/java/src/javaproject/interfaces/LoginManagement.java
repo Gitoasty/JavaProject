7affb654650f8a1ac40e0c164786edffc24a4382
Gitoasty <65080822+Gitoasty@users.noreply.github.com>
2025-01-24 15:12:49 +0100

    commit

--- src/main/java/src/javaproject/interfaces/LoginManagement.java
+++ src/main/java/src/javaproject/interfaces/LoginManagement.java
@@ -1,9 +1,5 @@
 package src.javaproject.interfaces;
 
-import src.javaproject.classes.Account;
-
-import java.io.FileInputStream;
-
 /**
  * Interface for handling logging into the app and creation of new accounts
  */
